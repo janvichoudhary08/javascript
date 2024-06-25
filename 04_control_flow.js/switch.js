6a90d35576865101a94a3b3f3755da94c00f68f2
Janvi choudhary <112813157+janvichoudhary08@users.noreply.github.com>
2024-06-25 06:19:38 +0000

    commit

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,31 @@
+// one value checked with multiple conditions
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {           // shift + alt + down key
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+    
