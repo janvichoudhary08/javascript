d8ad123ccb0348112ec47e8b28fb94d16c3efb41
Janvi choudhary <112813157+janvichoudhary08@users.noreply.github.com>
2024-06-25 07:48:00 +0000

    commit

---
+++ 05_iterations.js/five.js
@@ -0,0 +1,42 @@
+
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+//for each
+
+// coding.forEach( function (val){     callback function
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {    arrow function
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {   array list 
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
